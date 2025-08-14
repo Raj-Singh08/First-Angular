860e80ba9a723f26e9a58783995235c8e2141f00
Raj-Singh08 <vimal38051@gmail,com>
2025-08-14 15:23:33 +0530

    Header Component Created and Assest folder

--- First-Angular/src/app/app.ts
+++ First-Angular/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component'; // Importing the HeaderComponent
+import { User } from './user/user'; // Importing the User component
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  selector: 'app-root',     // This is the root component of the application(Tells that this element needs to be replaced with this component)
+  standalone: true,        // Indicates that this component is a standalone component
+  imports: [HeaderComponent,User], // Importing the HeaderComponent to use it in this component
+  templateUrl: './app.html',  // The HTML template for this component
   styleUrl: './app.css'
 })
 export class App {
