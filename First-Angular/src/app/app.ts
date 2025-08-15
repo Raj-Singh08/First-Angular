59ec388a1df7e41cd3f262dcef91b3b37bae6d68
Raj-Singh08 <vimal38051@gmail,com>
2025-08-15 22:59:22 +0530

    Implement user selection and task display functionality with dummy data

--- First-Angular/src/app/app.ts
+++ First-Angular/src/app/app.ts
@@ -1,14 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component'; // Importing the HeaderComponent
 import { User } from './user/user'; // Importing the User component
+import { DUMMY_USERS } from './dummy-users'; // Importing dummy users data
+import { Tasks } from './tasks/tasks';
+import { NgFor , NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',     // This is the root component of the application(Tells that this element needs to be replaced with this component)
   standalone: true,        // Indicates that this component is a standalone component
-  imports: [HeaderComponent,User], // Importing the HeaderComponent to use it in this component
+  imports: [HeaderComponent, User, Tasks, NgFor, NgIf], // Importing the HeaderComponent to use it in this component
   templateUrl: './app.html',  // The HTML template for this component
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HotelInventory');
+  users = DUMMY_USERS;
+  selectedUserId? :string;
+
+  get selectedUser(){
+    return this.users.find(user => user.id === this.selectedUserId); 
+  }
+
+  onSelectUser(id:string){ 
+     this.selectedUserId = id; // Update the selected user ID when a user is selected
+}
 }
--- First-Angular/src/app/user/user.ts
+++ First-Angular/src/app/user/user.ts
@@ -1,7 +1,16 @@
-import { Component , signal , computed} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component ,Input, input, Output, EventEmitter, signal , computed , output} from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// type UserType = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface UserType {
+  id: string;
+  avatar: string;
+  name: string;
+}   
 
 @Component({
   selector: 'app-user',
@@ -10,19 +19,44 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  //selectedUser = DUMMY_USERS[randomIndex]; //selectedUser is now accessible in the template
-  selectedUser = signal(DUMMY_USERS[randomIndex]); // Using signal to make selectedUser reactive
 
-  imagePath = computed(() => 'assets/users/'+this.selectedUser().avatar); // Using computed to derive the image path reactively
+  @Input({required: true}) user!:UserType;
 
-  // get imagePath() {
-  //   return 'assets/users/'+this.selectedUser.avatar;  //get imagePath is a getter that returns the path to the user's avatar image
-  // }
+  // @Input({required: true}) id: string | undefined; 
+  //  @Input({required: true}) avatar!: string; //Input decorator use is to pass data from parent component to child component
+  //  @Input({required: true}) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();    //readonly input properties and connot be changed by the child component , create inputSignal for each input property
+  
+
+
+  @Output() select = new EventEmitter<string>(); 
+// In user.html:
+// The button calls onSelectUser() when clicked.
+// In user.ts:
+// onSelectUser() emits the select event with the user's id.
+// In app.html:
+// The (select) event on <app-user> calls onSelectUser($event) in the parent.
+// In app.ts:
+// onSelectUser(id: string) logs the selected user ID.
+
+
+  //select = output<string>();    // underthe hood, this is an EventEmitter<string> that emits the user's id when selected
+
+
+   get imagePath() {
+     return `assets/users/${this.user.avatar}`;
+   }
+
+  //imagePath = computed(() => `assets/users/${this.avatar()}`);  //we always read signal values using the function call syntax, like this: signalName()
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]); // Update the selected user using signal
-    //this.selectedUser = DUMMY_USERS[randomIndex]; 
+    this.select.emit(this.user.id); // Emit the select event when the user is selected
+
+    //Just for demonstration, randomly select a user from DUMMY_USERS
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]); // Update the selected user using signal
+    // //this.selectedUser = DUMMY_USERS[randomIndex]; 
   }
 }
  
