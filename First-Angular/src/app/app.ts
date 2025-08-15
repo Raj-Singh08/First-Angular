ef1a6e246383779289ef318000ce38a79a7b9b18
Raj-Singh08 <vimal38051@gmail,com>
2025-08-15 23:02:24 +0530

    removed ngFor and ngIf

--- First-Angular/src/app/app.ts
+++ First-Angular/src/app/app.ts
@@ -2,13 +2,12 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component'; // Importing the HeaderComponent
 import { User } from './user/user'; // Importing the User component
 import { DUMMY_USERS } from './dummy-users'; // Importing dummy users data
-import { Tasks } from './tasks/tasks';
-import { NgFor , NgIf} from '@angular/common';
+import { Tasks } from './tasks/tasks'; // Importing the Tasks component
 
 @Component({
   selector: 'app-root',     // This is the root component of the application(Tells that this element needs to be replaced with this component)
   standalone: true,        // Indicates that this component is a standalone component
-  imports: [HeaderComponent, User, Tasks, NgFor, NgIf], // Importing the HeaderComponent to use it in this component
+  imports: [HeaderComponent,User,Tasks], // Importing the HeaderComponent to use it in this component
   templateUrl: './app.html',  // The HTML template for this component
   styleUrl: './app.css'
 })
