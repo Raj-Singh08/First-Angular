f5e5d2922e8f8cb5ec5e3e1b1604a75545e704d3
Raj-Singh08 <vimal38051@gmail,com>
2025-08-15 10:41:27 +0530

    State Management through zone.js and Signal

--- First-Angular/src/app/user/user.ts
+++ First-Angular/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , signal , computed} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,6 +10,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex]; //selectedUser is now accessible in the template
+  //selectedUser = DUMMY_USERS[randomIndex]; //selectedUser is now accessible in the template
+  selectedUser = signal(DUMMY_USERS[randomIndex]); // Using signal to make selectedUser reactive
+
+  imagePath = computed(() => 'assets/users/'+this.selectedUser().avatar); // Using computed to derive the image path reactively
+
+  // get imagePath() {
+  //   return 'assets/users/'+this.selectedUser.avatar;  //get imagePath is a getter that returns the path to the user's avatar image
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]); // Update the selected user using signal
+    //this.selectedUser = DUMMY_USERS[randomIndex]; 
+  }
 }
  
--- First-Angular/src/main.ts
+++ First-Angular/src/main.ts
@@ -1,4 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
 import { App } from './app/app';
 import { HeaderComponent } from './app/header/header.component';
 
